b70b3425d99b7d8e5c5faac32f9a2c6a392363de
inawashiro <38335846+inawashiro@users.noreply.github.com>
2018-10-03 12:11:29 +0900

    Add files via upload

--- laplace_experiment.py
+++ laplace_experiment.py
@@ -283,23 +283,23 @@ class Experiment(laplace_theory.Theory):
         f[4] = linear_laplacian_u[1]
         f[5] = linear_laplacian_u[2]
         
-        unknown_init = ((1 + random.uniform(-10.0, 10.0)/100)*a_theory[3],
-                        (1 + random.uniform(-10.0, 10.0)/100)*a_theory[4],
-                        (1 + random.uniform(-10.0, 10.0)/100)*a_theory[5],
-                        (1 + random.uniform(-10.0, 10.0)/100)*b_theory[3],
-                        (1 + random.uniform(-10.0, 10.0)/100)*b_theory[4],
-                        (1 + random.uniform(-10.0, 10.0)/100)*b_theory[5]
-                        )
-        error = np.ndarray((len(f),))
+        solution = ((1 + random.uniform(-10.0, 10.0)/100)*a_theory[3],
+                    (1 + random.uniform(-10.0, 10.0)/100)*a_theory[4],
+                    (1 + random.uniform(-10.0, 10.0)/100)*a_theory[5],
+                    (1 + random.uniform(-10.0, 10.0)/100)*b_theory[3],
+                    (1 + random.uniform(-10.0, 10.0)/100)*b_theory[4],
+                    (1 + random.uniform(-10.0, 10.0)/100)*b_theory[5]
+                    )
+        
+        error = np.ndarray((len(f),), 'object')
         for l in range(len(f)):
-            f[l] = lambdify(unknown, f[l], 'numpy')
-            f[l] = f[l](unknown_init[0],
-                        unknown_init[1],
-                        unknown_init[2],
-                        unknown_init[3],
-                        unknown_init[4],
-                        unknown_init[5])
-            error[l] = f[l]
+            error[l] = lambdify(unknown, f[l], 'numpy')
+            error[l] = error[l](solution[0],
+                                solution[1],
+                                solution[2],
+                                solution[3],
+                                solution[4],
+                                solution[5])
         while np.linalg.norm(error) > 1.0e-4:
             A = np.ndarray((len(f), len(unknown),), 'object')
             b = np.ndarray((len(f),), 'object')
@@ -312,36 +312,36 @@ class Experiment(laplace_theory.Theory):
                        + diff(f[j], unknown[4])*unknown[4] \
                        + diff(f[j], unknown[5])*unknown[5]
                 b[j] = lambdify(unknown, b[j], 'numpy')
-                b[j] = b[j](unknown_init[0],
-                            unknown_init[1],
-                            unknown_init[2],
-                            unknown_init[3],
-                            unknown_init[4],
-                            unknown_init[5]
+                b[j] = b[j](solution[0],
+                            solution[1],
+                            solution[2],
+                            solution[3],
+                            solution[4],
+                            solution[5]
                             )
                 for k in range(len(unknown)):
                     A[j][k] = diff(f[j], unknown[k])
                     A[j][k] = lambdify(unknown, A[j][k], 'numpy')
-                    A[j][k] = A[j][k](unknown_init[0],
-                                      unknown_init[1],
-                                      unknown_init[2],
-                                      unknown_init[3],
-                                      unknown_init[4],
-                                      unknown_init[5]
+                    A[j][k] = A[j][k](solution[0],
+                                      solution[1],
+                                      solution[2],
+                                      solution[3],
+                                      solution[4],
+                                      solution[5]
                                       )
             A = A.astype('float')
             b = b.astype('float')
-            solution = np.linalg.lstsq(A, b)[0]
-            unknown_init = solution
+#            solution = np.linalg.lstsq(A, b)[0]
+            solution = np.linalg.solve(A, b)
             for l in range(len(f)):
-                f[l] = lambdify(unknown, f[l], 'numpy')
-                f[l] = f[l](unknown_init[0],
-                            unknown_init[1],
-                            unknown_init[2],
-                            unknown_init[3],
-                            unknown_init[4],
-                            unknown_init[5])
-                error[l] = f[l]
+                error[l] = lambdify(unknown, f[l], 'numpy')
+                error[l] = error[l](solution[0],
+                                    solution[1],
+                                    solution[2],
+                                    solution[3],
+                                    solution[4],
+                                    solution[5])
+        
         return solution
 
         
@@ -393,7 +393,6 @@ if __name__ == '__main__':
     
     unknown_experiment = experiment.solution()
     print('(a_experiment, b_experiment) = ')
-#    print(unknown_experiment)
     [print(round(item, 4)) for item in unknown_experiment]
     print('')
     
@@ -428,7 +427,6 @@ if __name__ == '__main__':
     print('Elapsed Time = ', round(t1 - t0), '(s)')
         
     
-
 
 
 
